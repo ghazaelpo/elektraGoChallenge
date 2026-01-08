70a51c6f1a689c69c961487466e7dc5f474fbab4
ghazaelpo <genarohazael@gmail.com>
2026-01-08 11:00:34 -0700

    deploying a simple hello world

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,6 @@
 FROM public.ecr.aws/lambda/python:3.9
 
-# Copiamos el código
 COPY app/main.py ${LAMBDA_TASK_ROOT}
-COPY app/requirements.txt .
 
-# Instalamos dependencias
-RUN pip install -r requirements.txt
-
+# No instalamos nada porque no hay dependencias
 CMD [ "main.handler" ]
--- app/main.py
+++ app/main.py
@@ -1,14 +1,6 @@
-from flask import Flask
-from mangum import Mangum # Este es el adaptador para Lambda
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return {"message": "Hola Magdaleno"}, 200
-
-# Esta es la variable que Lambda buscará (definida en el CMD como main.handler)
-handler = Mangum(app)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+def handler(event, context):
+    print("Hola desde los logs!") # Esto saldrá en CloudWatch
+    return {
+        'statusCode': 200,
+        'body': 'Hola Mundo'
+    }
