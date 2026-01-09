37c8e0505bf8af31b7072a05b20e98d472de2628
magdaleno <magdaleno.garcia@fraternitas.io>
2026-01-08 19:34:19 -0600

    Test case 1

--- app/main.py
+++ app/main.py
@@ -2,5 +2,5 @@ def handler(event, context):
     print("Hola desde los logs!") # Esto saldrá en CloudWatch
     return {
         'statusCode': 200,
-        'body': 'Hola Mundo'
+        'body': 'Hola Mundo, Test case 1'
     }
